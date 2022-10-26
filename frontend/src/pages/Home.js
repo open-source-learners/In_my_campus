e4de3850832178c8071451ef65c3aa9d35bd2d11
Arman Grema <abdurrahmangrema19@gmail.com>
2022-10-26 07:35:57 -1100

    added pages

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Outlet} from "react-router-dom"
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Events page componet</h1>
+        </div>
+    )
+}
